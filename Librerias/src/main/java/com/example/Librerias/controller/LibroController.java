b38a9a6755c40262fa9470d46b26cba64f7b9b51
Areliz <are.isla@duocuc.cl>
2025-04-09 21:06:13 -0400

    se intenta agregar clases xd

--- Librerias/src/main/java/com/example/Librerias/controller/LibroController.java
+++ Librerias/src/main/java/com/example/Librerias/controller/LibroController.java
@@ -1,5 +1,31 @@
 package com.example.Librerias.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.Librerias.model.Libro;
+import com.example.Librerias.repository.LibroRepository;
+import com.example.Librerias.service.*; // aqui falta anotar la clase de java importada
+
+
+@RestController
+@RequestMapping("api/v1/libros")
+
 public class LibroController {
         // aqui falta mucho codigo
+        @Autowired
+        private Libro libroService;
+
+        @GetMapping
+        public List<Libro> getLibro;
 }
---
+++ Librerias/src/main/java/com/example/Librerias/service/LibroService.java
@@ -0,0 +1,13 @@
+package com.example.Librerias.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.*; //falta codigo
+
+@Service
+public class LibroService {
+    @Autowired
+
+    
+}
